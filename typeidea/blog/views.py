0a55ba145ea1302c2414d15dd5711605eefd5581
andrew <andrew@test.com>
2023-05-01 05:38:08 +0800

    升级至class-base view 将post_list 和post_detail 函数改造成class-base view 增加：CommonViewMixin，IndexView，CategoryView，TagView，PostDetailView 修改对应的url和模板

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,45 +1,71 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Post, Tag, Category
 from config.models import SideBar
+from django.views.generic import DetailView, ListView
 
 
-def post_list(request, category_id=None, tag_id=None):
-    tag = None
-    category = None
-
-    if tag_id:
-        post_list, tag = Post.get_by_tag(tag_id)
-    elif category_id:
-        post_list, category = Post.get_by_category(category_id)
-    else:
-        post_list = Post.latest_posts()
-
-    """
-    # print("post list final:", post_list)
-     post list final: <QuerySet [<Post: test用户的标题2>, <Post: 标题测试test>]>
-    """
-    context = {
-        'category': category,
-        'tag': tag,
-        'post_list': post_list,
-        'sidebars': SideBar.get_all()
-    }
-    context.update(Category.get_navs())
-    # print("context:", context)
-
-    return render(request, 'blog/list.html', context=context)
+class CommonViewMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {
+                'sidebars': SideBar.get_all(),
+            }
+        )
+        context.update(Category.get_navs())
+        return context
 
 
-def post_detail(request, post_id):
-    try:
-        post = Post.objects.get(id=post_id)
-    except Post.DoesNotExist:
-        post = None
+class IndexView(CommonViewMixin, ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
 
-    context = {
-        'post': post,
-        'sidebars': SideBar.get_all()
-    }
-    context.update(Category.get_navs())
-    return render(request, 'blog/detail.html', context=context)
+
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        #  get_object_or_404是一个快捷方式，用来获取一个对象实例，如果获取到就返回实例对象，如果不存在，则直接抛出404错误
+        category = get_object_or_404(Category, pk=category_id)
+        context.update(
+            {
+                'category': category,
+            }
+        )
+        return context
+
+    def get_queryset(self):
+        """重写queryset，根据分类过滤"""
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update(
+            {
+                'tag': tag,
+            }
+        )
+        return context
+
+    def get_queryset(self):
+        """重写queryset，根据标签过滤"""
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag_id=tag_id)
+
+
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
 

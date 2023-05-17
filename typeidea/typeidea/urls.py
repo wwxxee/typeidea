b20c036bca1b1f407b8af3acab68587fcca32b8d
wwxxee <wwxxee@outlook.com>
2023-05-17 21:58:25 +0800

    增加友链页面 config/views.py
    class LinkListView
    config/link.html

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -17,17 +17,18 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from blog.views import (IndexView, CategoryView, TagView, PostDetailView, SearchView, AuthorView)
-from config.views import links
+from config.views import LinkListView
 from .custom_site import custom_site
+
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
     url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
     url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
-    url(r'^links/$', links, name='links'),
     url(r'^search/$', SearchView.as_view(), name='search'),
     url(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name='author'),
+    url(r'^links/$', LinkListView.as_view(), name='links'),
 
     url(r'^super_admin/', admin.site.urls, name='super-admin'),
     url(r'^admin/', custom_site.urls, name='admin')

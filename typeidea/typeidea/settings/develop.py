3d8fc00360f0895df9b0d1180c6cba4f8b1b8385
wwxxee <wwxxee@outlook.com>
2023-07-11 00:03:21 +0800

    配置pymysql

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -5,8 +5,14 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jowon_blog_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '192.168.52.133',
+        'PORT': 3306,
+        'CONN_MAX_AGE': 5 * 60,
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
---
+++ typeidea/typeidea/settings/product.py
@@ -0,0 +1,19 @@
+from .base import *  # NOQA
+
+
+# DEBUG = False
+#
+# ALLOWED_HOSTS = ['*']
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'jowon_blog_db',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': '192.168.52.133',
+#         'PORT': 3306,
+#         'CONN_MAX_AGE': 5 * 60,
+#         'OPTIONS': {'charset': 'utf8mb4'},
+#     }
+# }
